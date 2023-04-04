b7df291c539932fc8b1c9797b24234e3f998aea7
Riki321 <rickyparikh35@gmail.com>
2023-04-04 19:56:26 +0530

    first

--- resource/routes/resource.js
+++ resource/routes/resource.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const recourseController = require('../controllers/resourceController');
 
 router.post("/",recourseController.Resource_post);
-
+//
 module.exports = router;
